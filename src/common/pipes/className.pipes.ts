d4ce1e90a689217f5a7a7fe612ba8df211dddcec
longnh <longnh.uit@gmail.com>
2024-12-18 14:22:19 +0700

    feat: change classes.pipe.ts -> className.pipe.ts

--- src/common/pipes/className.pipes.ts
+++ src/common/pipes/className.pipes.ts
@@ -1,16 +1,7 @@
 import { PipeTransform, Injectable, ArgumentMetadata, HttpException, HttpStatus } from '@nestjs/common';
 import { CLASS_EXISTS, CLASS_NAME_REQUIRED, CLASS_NAME_UNVALID } from '../errors/constants.errors';
-import { ClassEntity } from 'src/classes/entities/class.entity';
-import { listClasses } from 'src/data/data';
+import { ContainsSpecificSpecialChars, FindClassbyName } from '../helper/classess.helper';
 
-const ContainsSpecificSpecialChars = (str: string): boolean => {
-  const allowedChars = /^[a-zA-Z0-9_\.]+$/;
-  return !allowedChars.test(str);
-};
-
-const FindClass = (className: string): ClassEntity => {
-  return listClasses.find(cls => cls.getClassName == className);
-};
 @Injectable()
 export class ClassesPipe implements PipeTransform<{ className: string }> {
   transform(value: { className: string }, metadata: ArgumentMetadata) {
@@ -20,7 +11,7 @@ export class ClassesPipe implements PipeTransform<{ className: string }> {
     } else {
       if (ContainsSpecificSpecialChars(value.className))
         throw new HttpException(CLASS_NAME_UNVALID, HttpStatus.BAD_REQUEST);
-      if (FindClass(value.className)) throw new HttpException(CLASS_EXISTS, HttpStatus.BAD_REQUEST);
+      if (FindClassbyName(value.className)) throw new HttpException(CLASS_EXISTS, HttpStatus.BAD_REQUEST);
     }
     // return value.className.toLowerCase().trim();
     return value;
