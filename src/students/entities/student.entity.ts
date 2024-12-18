92ac70224229a0d075f83f8a1020d4d5a8ba8018
longnh <longnh.uit@gmail.com>
2024-12-18 19:03:05 +0700

    feat: update student entity

--- src/students/entities/student.entity.ts
+++ src/students/entities/student.entity.ts
@@ -1,1 +1,35 @@
-export class StudentEntity {}
+export class StudentEntity {
+  private studentId: number;
+  public studentName: string;
+  public className: string;
+
+  constructor(studentId: number, studentName: string, className: string) {
+    this.studentId = studentId;
+    this.studentName = studentName;
+    this.className = className;
+  }
+
+  getStudentId(): number {
+    return this.studentId;
+  }
+
+  setStudentId(studentId: number): void {
+    this.studentId = studentId;
+  }
+
+  getStudentName(): string {
+    return this.studentName;
+  }
+
+  setStudentName(studentName: string): void {
+    this.studentName = studentName;
+  }
+
+  getClassName(): string {
+    return this.className;
+  }
+
+  setClassName(className: string): void {
+    this.className = className;
+  }
+}
