032f64c74b0d6539e00a56aff618df56236aad1c
longnh <longnh.uit@gmail.com>
2024-12-18 19:23:04 +0700

    feat: add pipe to classes

--- src/classes/classes.controller.ts
+++ src/classes/classes.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Get, ParseIntPipe, Post, UseFilters } from '@nestjs/c
 import { ClassesService } from './classes.service';
 import { CreateClassDto } from './dto/create-class.dto';
 import { HttpExceptionFilter } from 'src/common/filters/http-exception.filter';
+import { ClassPipe } from 'src/common/pipes/class-pipe/class-pipe.pipe';
 
 @Controller('classes')
 @UseFilters(HttpExceptionFilter)
@@ -9,12 +10,12 @@ export class ClassesController {
   constructor(private readonly classesService: ClassesService) {}
 
   @Post()
-  create(@Body() createClassDto: CreateClassDto) {
+  create(@Body(ClassPipe) createClassDto: CreateClassDto) {
     return this.classesService.create(createClassDto);
   }
 
   @Get()
-  findOne(@Body(ParseIntPipe) classId: number) {
+  findOne(@Body(new ParseIntPipe({})) classId: number) {
     return this.classesService.findOne(classId);
   }
 }
---
+++ src/common/pipes/class-pipe/class-pipe.pipe.ts
@@ -0,0 +1,14 @@
+import { ArgumentMetadata, HttpException, HttpStatus, Injectable, PipeTransform } from '@nestjs/common';
+import { CLASS_EXISTS } from 'src/common/errors/constants.errors';
+import { listClasses } from 'src/data/data';
+
+@Injectable()
+export class ClassPipe implements PipeTransform {
+  transform(value: { className: string }, metadata: ArgumentMetadata) {
+    console.log(metadata);
+    if (listClasses.find(c => c.className === value.className)) {
+      throw new HttpException(CLASS_EXISTS, HttpStatus.BAD_REQUEST);
+    }
+    return value;
+  }
+}
