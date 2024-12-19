44cec2f4d6a5a01e71792edeb96d935cd3ea1b9d
longnh <longnh.uit@gmail.com>
2024-12-19 10:57:21 +0700

    fix: i dont know what i'm doing but i just 70%

--- src/common/pipes/update-student/update-student.pipe.ts
+++ src/common/pipes/update-student/update-student.pipe.ts
@@ -1,15 +1,23 @@
-import { ArgumentMetadata, HttpException, HttpStatus, Injectable, PipeTransform } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, PipeTransform } from '@nestjs/common';
 import { CLASS_NOT_FOUND, STUDENT_NOT_FOUND } from 'src/common/errors/constants.errors';
 import { FindClassbyName, FindIndexStudentById } from 'src/common/helper/helper.helper';
+import { listStudents } from 'src/data/data';
 
 @Injectable()
 export class UpdateStudentPipe implements PipeTransform {
-  transform(value: { studentId: number; className: string; studentName: string }, metadata: ArgumentMetadata) {
-    console.log(metadata);
+  transform(value: { studentId: number; className: string; studentName: string }) {
+    if (FindIndexStudentById(value.studentId) == -1) {
+      throw new HttpException(STUDENT_NOT_FOUND, HttpStatus.NOT_FOUND);
+    }
+
+    const index = FindIndexStudentById(value.studentId);
+    const currentStudent = listStudents[index];
+
+    if (value.studentName == '') value.studentName = currentStudent.getStudentName();
+    if (value.className == '') value.className = currentStudent.getClassName();
+
     if (!FindClassbyName(value.className)) {
       throw new HttpException(CLASS_NOT_FOUND, HttpStatus.NOT_FOUND);
-    } else if (FindIndexStudentById(value.studentId) == -1) {
-      throw new HttpException(STUDENT_NOT_FOUND, HttpStatus.NOT_FOUND);
     }
     return value;
   }
